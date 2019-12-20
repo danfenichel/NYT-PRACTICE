31f4567a472e14b6a8a30d8d4d309c3c780120d1
D0491 <dan.fenichel@yahoo.com>
2019-12-20 13:34:22 -0600

    created variables for all DOM elements and psuedo coded out how program will work

---
+++ script.js
@@ -0,0 +1,22 @@
+// DOM elements
+var searchEl = $("#search");
+var numRecordsEl = $("#number");
+var startYearEl = $("#start");
+var endYearEl = $("#end");
+var searchBtnEl = $("#searchBtn");
+var clearBtnEl = $("#clearBtn");
+var articlesEl = $("#articles");
+
+// Functions
+
+// Event listeners
+
+    // button click on the search button will activate the AJAX data pull
+        // set results limit to variable equal to the value chosen in the number 
+        // of records dropdown and add variable into URL string
+        // compare article "title" to search term(s)
+        // compare article "year" to both start year and end year [if entered]
+        // grab search results and append them to empty #articles div
+
+    // button click on clear results button
+        // empty out search results from #articles div
